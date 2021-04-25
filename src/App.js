34d75ad845d11fc38be93a727fbecb9798cb18bb
Lohit244 <lohit244@gmail.com>
2021-04-25 14:43:20 +0530

    Folder Structure Changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import "./App.css"
+import React from "react"
 import ConnectBar from "./components/ConnectBar/ConnectBar"
 import About from "./components/About/About"
-import headerBackground from "./res/429540.jpg"
-import { Parallax } from "react-parallax"
 import loadingGif from "./res/logo.gif"
 import { useEffect, useState } from "react"
-import alertimg from "./res/alert.png"
 import Projects from "./components/Projects/Projects"
 import LoadingScreen from "react-loading-screen"
-import Typewriter from "typewriter-effect"
+import SkillIntrest from "./components/SkillIntrest/SkillIntrest"
+import Landing from "./components/Landing/Landing.js"
 function App() {
 	const [isLoading, setIsLoading] = useState(true)
 	useEffect(() => {
@@ -16,8 +15,7 @@ function App() {
 			setIsLoading(false)
 		}, 2000)
 	}, [])
-
-	const [alertRead, setAlertRead] = useState(false)
+	
 	return (
 		<LoadingScreen
 			loading={isLoading}
@@ -26,50 +24,11 @@ function App() {
 			textColor="#FFFFFF"
 			logoSrc={loadingGif}
 			text="Please Scroll A Bit On Loading In"
-		>
+			>
 			<div className="App">
-				<Parallax bgImage={headerBackground} strength={300}>
-					{!alertRead && (
-						<div className="idk">
-							<img id="alertimg" src={alertimg} alt="!ALERT - "></img>
-							This is a website built during Lead 2.0 at BIT Mesra while i am
-							still learning a lot of things.
-							<button
-								id="x"
-								onClick={() => {
-									setAlertRead(true)
-								}}
-							>
-								x
-							</button>
-						</div>
-					)}
-					<div className="App-header">
-						<div className="big-text">I am Lohitaksha Malhotra.</div>
-						<div className="small-text">
-							I am a mechanical engineering student (1st Year) at BIT Mesra. I
-							like to dabble in various parts of frontend development and like
-							to learn about various technologies. I usually look into these or
-							just play some games during my free time
-						</div>
-						<div className="notsobig-text">
-						<Typewriter
-							options={{
-								strings: ["Programmer", "Developer", "Cool Guy" , "Learner", "Thinker"],
-								autoStart: true,
-								loop: true,
-							}}
-						/>
-						</div>
-						<div className="small-text" id = "yooloo">
-							scroll down
-						</div>
-						<div className="small-text" id="arrow">
-							^
-						</div>
-					</div>
-				</Parallax>
+				<Landing/>
 				<About />
+				<SkillIntrest/>
 				<Projects />
 				<div>
 					<hr id="Connect-Hr"></hr>
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,30 +2,30 @@ import React from "react"
 import "./About.css"
 import myphoto from "../../res/about.png"
 import { Parallax } from "react-parallax"
-import footer from "../../res/footer.jpg"
 import boom from "../../res/boom.png"
 function About() {
+	const boomPhoto = precentage => (
+		<div 
+		  style={{
+			position: 'absolute',
+			width: '100px',
+			height: '100px',
+			borderRadius: '50%',
+			backgroundImage: `url(${boom})`,
+			backgroundRepeat: 'no-repeat',
+			backgroundSize:`cover`,
+			left: '50%',
+			top: '50%',
+			transform: `translate(-50%, -50%) scale(${precentage * 7})`,
+		  }}
+		>
+		</div>
+	  )
 	return (
 		<div className="about-container">
 			<div className="about">
 				<Parallax 
-				renderLayer={precentage => (
-          <div 
-            style={{
-              position: 'absolute',
-              width: '100px',
-              height: '100px',
-              borderRadius: '50%',
-              backgroundImage: `url(${boom})`,
-			  backgroundRepeat: 'no-repeat',
-			  backgroundSize:`cover`,
-              left: '50%',
-              top: '50%',
-              transform: `translate(-50%, -50%) scale(${precentage * 7})`,
-            }}
-          >
-          </div>
-        )}>
+				renderLayer={boomPhoto}>
 			<div className = "photo-block">
 					<img id="my-img" src={myphoto} alt="something went wrong"></img>
 					<div className = "photo-text">Lohitaksha Malhotra</div>
@@ -42,54 +42,7 @@ function About() {
 					Mechanical Engineering - BIT Mesra (Currently Persuing)
 				</div>
 			</div>
-			<Parallax bgImage={footer} strength={500}>
-				<div className="intrests">
-					<div className="about-header">
-						<h1 id="skillHeading">My Skills</h1>I am very new so i dont have
-						many but i'll keep improving
-					</div>
-					<div className="skills-container">
-						<div className="skill" id="python">
-							<h1>Python</h1>I now basic python and am working my way to
-							learning data science methods.
-						</div>
-						<div className="skill" id="react">
-							<h1>ReactJS</h1>
-							This entire project is built on react.. but ofcourse i am still
-							figuring my way with react too and am still but a newbie.
-						</div>
-						<div className="skill" id="webdev">
-							<h1>HTML and CSS</h1>
-							By no means exceptional at these and especially CSS, but i can deal
-							with these languages to an extent.
-						</div>
-						<div className="skill" id="photoshop">
-							<h1>Photoshop</h1>I have been using photoshop for quite some time
-							and can do a good bit with it
-						</div>
-					</div>
-				</div>
-				<div className="intrests">
-					<div className="about-header">
-						<h1 id="intrestHeading">My Intrests</h1>
-						in case you wanna know more about me
-					</div>
-					<div className="intrests-container">
-						<div className="topic" id="programming">
-							<h1>Programming</h1>I am still learning but ofcourse I do like
-							programming.
-						</div>
-						<div className="topic" id="manga">
-							<h1>Anime & Manga</h1>
-							Yes, i am a weeb.
-						</div>
-						<div className="topic" id="gaming">
-							<h1>Gaming</h1>I dont have a good gaming grade laptop but i do
-							still play games like Valorant and CS:GO.
-						</div>
-					</div>
-				</div>
-			</Parallax>
+			
 		</div>
 	)
 }
